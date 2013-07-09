486145c7559132178da6712859bb287d11c9f418
dominikknoll <dok@foryouandyourcustomers.com>
2013-07-09 09:26:10 +0200

    1

--- html/buildHeader.php
+++ html/buildHeader.php
@@ -1,14 +1,17 @@
 <div class="navbar">
-					<div class="navbar-inner">
-						<ul class="nav">
-							<li class="active"><a href="#">Referenzen</a></li>
-							<li><a href="#">Leistungen</a></li>
-							<li><a href="#">Standorte</a></li>
-							<li><a href="#" id="naviSlideButtonMenschen">Menschen</a></li>
-							<li><a href="#">Blog</a></li>
-						</ul>			    
-						<ul class="nav pull-right">	
-							<li><a href="#">de en</a></li>
-						</ul>
-					</div>
-				</div>
+	<div class="navbar-inner">
+		<ul class="nav">
+			<li class="active"><a href="#">Referenzen</a></li>
+			<li><a href="#">Leistungen</a></li>
+			<li><a href="#">Standorte</a></li>
+			<li><a href="#" id="naviSlideButtonMenschen">Menschen</a></li>
+			<li><a href="#">Blog</a></li>
+		</ul>			    
+		<ul class="nav pull-right">	
+			<li><a href="#">de en</a></li>
+		</ul>
+	</div>
+	<div class="navbar-shadow">
+	&nbsp;
+	</div>
+</div>
